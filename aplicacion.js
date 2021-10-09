64c08ef590920e2f6b6ffbf7f25465a5748985da
Alcides Blandón <luisalcidesblandon@gmail.com>
2021-10-09 13:27:57 -0600

    recibiendo los datos que tenemos en nuestra bds

--- aplicacion.js
+++ aplicacion.js
@@ -1,9 +1,19 @@
+const url ="http://localhost/CRUD_JS/";
+
 const aplicacion = new function() {
     this.nombre= document.getElementById('nombre');
     this.correo= document.getElementById('correo');
     this.empleados = document.getElementById('empleados');
+
     this.leer = function(){
         const datos='<tr><td>1</td><td>Juan</td><td>jua@gmail.com</td><td>Editar | Borrar</td></tr>';
+        
+        fetch(url)
+        .then(r=>r.json())
+        .then((respuesta) => {
+            console.log(respuesta);
+        })
+        .catch(console.log);
 
         return this.empleados.innerHTML=datos;
     }
