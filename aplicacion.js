cdfa03f79819d0da1058d3368eaec9312ffbe532
Alcides Blandón <luisalcidesblandon@gmail.com>
2021-10-09 13:19:28 -0600

    conexion a la base de datos

--- aplicacion.js
+++ aplicacion.js
@@ -1,9 +1,16 @@
 const aplicacion = new function() {
+    this.nombre= document.getElementById('nombre');
+    this.correo= document.getElementById('correo');
     this.empleados = document.getElementById('empleados');
     this.leer = function(){
         const datos='<tr><td>1</td><td>Juan</td><td>jua@gmail.com</td><td>Editar | Borrar</td></tr>';
 
         return this.empleados.innerHTML=datos;
+    }
+    this.agregar = function(){
+        console.log(nombre.value);
+        console.log(correo.value);
+
     }
 
 }
